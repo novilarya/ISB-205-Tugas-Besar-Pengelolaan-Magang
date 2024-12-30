086fc57554084e5b84dd81eaded11a434cc61be6
novilarya <arya.novila@gmail.com>
2024-12-30 20:52:49 +0700

    Bagian Admin

---
+++ Tugas_Besar-Pengelolaan_Magang/src/main/java/org/itenas/oop/project/repository/ControllerLogin.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itenas.oop.project.repository;
+
+/**
+ *
+ * @author aryan
+ */
+public class ControllerLogin {
+    
+}
