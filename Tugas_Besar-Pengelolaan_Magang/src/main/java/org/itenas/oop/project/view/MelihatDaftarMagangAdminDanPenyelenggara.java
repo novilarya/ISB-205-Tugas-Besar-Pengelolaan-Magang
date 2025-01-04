836a4a7310b4b46f75bbe5f8288227bc94602529
novilarya <147460997+novilarya@users.noreply.github.com>
2025-01-05 00:27:57 +0700

    Update MelihatDaftarMagangAdminDanPenyelenggara.java
    fixing jpanel, jlabel

--- Tugas_Besar-Pengelolaan_Magang/src/main/java/org/itenas/oop/project/view/MelihatDaftarMagangAdminDanPenyelenggara.java
+++ Tugas_Besar-Pengelolaan_Magang/src/main/java/org/itenas/oop/project/view/MelihatDaftarMagangAdminDanPenyelenggara.java
@@ -356,12 +356,6 @@ public class MelihatDaftarMagangAdminDanPenyelenggara extends javax.swing.JFrame
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-
-                .addGap(28, 28, 28)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(143, Short.MAX_VALUE))
-        );
-
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Profile");
@@ -376,8 +370,6 @@ public class MelihatDaftarMagangAdminDanPenyelenggara extends javax.swing.JFrame
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 347, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -516,9 +508,5 @@ public class MelihatDaftarMagangAdminDanPenyelenggara extends javax.swing.JFrame
     private javax.swing.JTextArea jTextArea2;
     private javax.swing.JTextArea jTextArea3;
     private javax.swing.JTextArea jTextArea4;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
